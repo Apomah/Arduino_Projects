be9d406bdb0ecc405388a974f5a37025dc1ca449
Apomah <hockey.goalie1@live.ca>
2016-11-15 19:33:55 -0500

    Add files via upload

---
+++ 5x5x6_LED_Cube/Code/_5x5x6_cube_r_to_g.ino
@@ -0,0 +1,82 @@
+//For the purposes of this series of code, the entire cube will begin colour "A" and steadily become colour "B"
+
+//Initlizing the pins
+int row[]={1,2,3,4,5};
+int column[]={6,7,8,9,10};
+int layer[]={A0,A1,A2,A3,A4,A5};
+int pinCount=6;
+
+//Declaring the pins as output
+void setup(){
+  for(int thisPin = 0; thisPin < pinCount; thisPin++){
+    pinMode(row[thisPin],OUTPUT);
+    pinMode(column[thisPin],OUTPUT);
+    pinMode(layer[thisPin],OUTPUT);
+  }
+}
+
+//Now the actual code
+void loop(){
+  //The code which uses for loops and the above arrays
+    //Makes all enable pins low to transfer data then second for loop sets each row high and each column low to make all LEDs colour "A"
+    for(int thisPin = 0; thisPin < pinCount; thisPin++){
+      digitalWrite(layer[thisPin],LOW);
+    };
+    for(int thisPin = 0; thisPin < pinCount; thisPin++){
+      digitalWrite(row[thisPin],HIGH);
+      digitalWrite(column[thisPin],LOW);
+    };
+    delay(5000);
+   //Now to start the turning of colour "A" to colour "B". We begin by making all layers high for data will only be transfercolour "A" as we turn each layer back on
+   for(int thisPin = 0; thisPin < pinCount; thisPin++){
+      digitalWrite(layer[thisPin],HIGH);
+   };
+   //We continue by making layer 1 low. Then we use the for loop to gradually change each LED from colour "A" to colour "B"
+   digitalWrite(layer[0],LOW);
+   for(int thisPin = 0; thisPin < pinCount; thisPin++){
+      digitalWrite(row[thisPin],LOW);
+      digitalWrite(column[thisPin],HIGH);
+      delay(2000);
+   };
+   //We continue by making layer 2 low and the previous layer high. Then we use the for loop to gradually change each LED from colour "A" to colour "B"
+   digitalWrite(layer[0],HIGH);
+   digitalWrite(layer[1],LOW);
+   for(int thisPin = 0; thisPin < pinCount; thisPin++){
+      digitalWrite(row[thisPin],LOW);
+      digitalWrite(column[thisPin],HIGH);
+      delay(2000);
+   };
+   //We continue by making layer 3 low and the previous layer high. Then we use the for loop to gradually change each LED from colour "A" to colour "B"
+   digitalWrite(layer[1],HIGH);
+   digitalWrite(layer[2],LOW);
+   for(int thisPin = 0; thisPin < pinCount; thisPin++){
+      digitalWrite(row[thisPin],LOW);
+      digitalWrite(column[thisPin],HIGH);
+      delay(2000);
+   };
+   //We continue by making layer 4 low and the previous layer high. Then we use the for loop to gradually change each LED from colour "A" to colour "B"
+   digitalWrite(layer[2],HIGH);
+   digitalWrite(layer[3],LOW);
+   for(int thisPin = 0; thisPin < pinCount; thisPin++){
+      digitalWrite(row[thisPin],LOW);
+      digitalWrite(column[thisPin],HIGH);
+      delay(2000);
+   };
+   //We continue by making layer 5 low and the previous layer high. Then we use the for loop to gradually change each LED from colour "A" to colour "B"
+   digitalWrite(layer[3],HIGH);
+   digitalWrite(layer[4],LOW);
+   for(int thisPin = 0; thisPin < pinCount; thisPin++){
+      digitalWrite(row[thisPin],LOW);
+      digitalWrite(column[thisPin],HIGH);
+      delay(2000);
+   };
+   //We continue by making layer 6 low and the previous layer high. Then we use the for loop to gradually change each LED from colour "A" to colour "B"
+   digitalWrite(layer[4],HIGH);
+   digitalWrite(layer[5],LOW);
+   for(int thisPin = 0; thisPin < pinCount; thisPin++){
+      digitalWrite(row[thisPin],LOW);
+      digitalWrite(column[thisPin],HIGH);
+      delay(2000);
+   };
+}
+    
---
+++ 5x5x6_LED_Cube/Code/_5x5x6_cube_tree.ino
@@ -0,0 +1,119 @@
+//This creates a "christmas tree" with a "trunk" of colour A and "branches" of colour B and then continuously flips the colours
+int row[]={1,2,3,4,5};
+int column[]={6,7,8,9,10};
+int layer[]={A0,A1,A2,A3,A4,A5};
+int pinCount=6;
+
+//Declaring the pins as output
+void setup(){
+  for(int thisPin = 0; thisPin < pinCount; thisPin++){
+    pinMode(row[thisPin],OUTPUT);
+    pinMode(column[thisPin],OUTPUT);
+    pinMode(layer[thisPin],OUTPUT);
+  }
+}
+
+//Now the actual code
+void loop(){
+  //Makes all enable pins low to transfer data than second for loop sets each row high and each column high to make all LEDs off
+    for(int thisPin = 0; thisPin < pinCount; thisPin++){
+      digitalWrite(layer[thisPin],LOW);
+    };
+    for(int thisPin = 0; thisPin < pinCount; thisPin++){
+      digitalWrite(row[thisPin],HIGH);
+      digitalWrite(column[thisPin],HIGH);
+    };
+    
+  //Makes a "trunk" of colour A and "branches" of colour B
+  
+  //Makes all layers high for only individual layers will have data transferred
+    for(int thisPin = 0; thisPin < pinCount; thisPin++){
+      digitalWrite(layer[thisPin],HIGH);
+    };
+  //layer 1 to form the bottom of the "trunk" of the tree as colour A, consisting of middle 9 LEDs
+    digitalWrite(layer[0],LOW);
+    for(int thisPin = 1; thisPin < 4; thisPin++){
+      digitalWrite(row[thisPin],LOW);
+      digitalWrite(column[thisPin],HIGH);
+    };
+  //layer 2 to form next layer as part of the "trunk" of the tree as colour A, consisting of middle 9 LEDs
+    digitalWrite(layer[0],HIGH);
+    digitalWrite(layer[1],LOW);
+    for(int thisPin = 1; thisPin < 4; thisPin++){
+      digitalWrite(row[thisPin],LOW);
+      digitalWrite(column[thisPin],HIGH);
+    };
+  //layer 3 to form next layer as bottom of the "branches" part of the tree as colour B, consisting of middle 9 LEDs
+    digitalWrite(layer[1],HIGH);
+    digitalWrite(layer[2],LOW);
+    for(int thisPin = 1; thisPin < 4; thisPin++){
+      digitalWrite(row[thisPin],HIGH);
+      digitalWrite(column[thisPin],LOW);
+    };
+  //layer 4 to form next layer as the next portion of the "branches" part of the tree as colour B, consisting of all LEDs
+    digitalWrite(layer[2],HIGH);
+    digitalWrite(layer[3],LOW);
+    for(int thisPin = 0; thisPin < pinCount; thisPin++){
+      digitalWrite(row[thisPin],HIGH);
+      digitalWrite(column[thisPin],LOW);
+    };
+  //layer 5 to form next layer as the next portion of the "branches" part of the tree as colour B, consisting of middle 9 LEDs
+    digitalWrite(layer[3],HIGH);
+    digitalWrite(layer[4],LOW);
+    for(int thisPin = 1; thisPin < 4; thisPin++){
+      digitalWrite(row[thisPin],HIGH);
+      digitalWrite(column[thisPin],LOW);
+    };
+  //layer 6 to form next layer as the top of the "branches" part of the tree as colour B, consisting of middle 1 LED
+    digitalWrite(layer[4],HIGH);
+    digitalWrite(layer[5],LOW);
+    digitalWrite(row[2],HIGH);
+    digitalWrite(column[2],LOW);
+  //Pauses for 5 seconds
+    delay(5000);
+    
+  //This section flips the colours around
+  
+  //layer 1 to form the bottom of the "trunk" of the tree as colour B, consisting of middle 9 LEDs
+    digitalWrite(layer[5],HIGH);
+    digitalWrite(layer[0],LOW);
+    for(int thisPin = 1; thisPin < 4; thisPin++){
+      digitalWrite(row[thisPin],HIGH);
+      digitalWrite(column[thisPin],LOW);
+    };
+  //layer 2 to form next layer as part of the "trunk" of the tree as colour B, consisting of middle 9 LEDs
+    digitalWrite(layer[0],HIGH);
+    digitalWrite(layer[1],LOW);
+    for(int thisPin = 1; thisPin < 4; thisPin++){
+      digitalWrite(row[thisPin],HIGH);
+      digitalWrite(column[thisPin],LOW);
+    };
+  //layer 3 to form next layer as bottom of the "branches" part of the tree as colour A, consisting of middle 9 LEDs
+    digitalWrite(layer[1],HIGH);
+    digitalWrite(layer[2],LOW);
+    for(int thisPin = 1; thisPin < 4; thisPin++){
+      digitalWrite(row[thisPin],LOW);
+      digitalWrite(column[thisPin],HIGH);
+    };
+  //layer 4 to form next layer as the next portion of the "branches" part of the tree as colour A, consisting of all LEDs
+    digitalWrite(layer[2],HIGH);
+    digitalWrite(layer[3],LOW);
+    for(int thisPin = 0; thisPin < pinCount; thisPin++){
+      digitalWrite(row[thisPin],LOW);
+      digitalWrite(column[thisPin],HIGH);
+    };
+  //layer 5 to form next layer as the next portion of the "branches" part of the tree as colour A, consisting of middle 9 LEDs
+    digitalWrite(layer[3],HIGH);
+    digitalWrite(layer[4],LOW);
+    for(int thisPin = 1; thisPin < 4; thisPin++){
+      digitalWrite(row[thisPin],LOW);
+      digitalWrite(column[thisPin],HIGH);
+    };
+  //layer 6 to form next layer as the top of the "branches" part of the tree as colour A, consisting of middle 1 LED
+    digitalWrite(layer[4],HIGH);
+    digitalWrite(layer[5],LOW);
+    digitalWrite(row[2],LOW);
+    digitalWrite(column[2],HIGH);
+  //Pauses for 5 seconds
+    delay(5000);
+};
